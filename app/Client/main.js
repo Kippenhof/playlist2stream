50f6639622e6003c6cbe73bcd198dccde44b3f95
Kippenhof <54744977+Kippenhof@users.noreply.github.com>
2022-03-02 23:19:51 +0100

    added Static 1st Page, initialized Node.JS

---
+++ app/Client/main.js
@@ -0,0 +1,6 @@
+// Change "{}" to your options:
+// https://github.com/sampotts/plyr/#option
+const player = new Plyr('#player', {"title": "playlist2stream"});
+// Expose player so it can be used from the console
+window.player = player;
+
