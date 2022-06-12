8d4dceb91c36f595d4905cd58ae7b2fb7aa4ad3d
Kippenhof <54744977+Kippenhof@users.noreply.github.com>
2022-06-12 03:23:14 +0200

    initialized plyr with CDN, Implemented Basic Theming and Setup

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ var invidiousApi = require('./invidiousApiConnector.js');
 const express = require("express");
 const path = require("path");
 
+const player = require("plyr");
 const port = process.env.PORT || "8080";
 const app = express();
 
-invidiousApi.getPlaylist();
+var videoData = invidiousApi.getPlaylist();
+
+
+
+
 
 app.get('/', function(req, res) {
   app.use(express.static(__dirname + '/public'));
