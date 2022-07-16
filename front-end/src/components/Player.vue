8a6c4231520e81d9ea810d12b902df28396bf9ff
Kippenhof <54744977+Kippenhof@users.noreply.github.com>
2022-07-16 17:20:17 +0200

    Added Player Component

---
+++ front-end/src/components/Player.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="plyr__video-embed" id="player">
+        <iframe
+            src="https://www.youtube.com/embed/{{ playingVideo }}?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1"
+            allowfullscreen
+            allowtransparency
+            allow="autoplay"
+        ></iframe>
+    </div>
+</template>
+<script>
+
+    export default {
+        name:'Plyr',
+        data () {
+
+    },
+        methods: {
+        setPlaying() {
+// Collect Current Video From Playlist module            
+
+        }
+    }
+
+    }
+
+
+
+
+</script>
+<style>
+ h1 {
+     margin-top: 1%;
+ }
+ ul {
+    list-style-type: none;
+    text-align: left;
+}
+</style>
---
+++ front-end/src/components/Playlist.vue
@@ -0,0 +1,51 @@
+<template>
+    <ul>
+        <!--test in Funktion umwandeln und diese zum prüfen des Playing States nutzen -->
+        <li v-for="video in playlist" :key="video.title"> {{ video.title }} <button @click="setPlayingStatus(video.id, playlist)"> YES </button> </li>
+    </ul>
+</template>
+<script>
+
+    export default {
+        name:'VuePlaylist',
+        data () {
+        return {
+          playlist: [
+            {id: 1, title: 'Me at the Zoo', playing: false},
+            {id: 2, title: 'Gerhardt Warmduscher', playing: true}
+          ]
+        }
+    },
+        methods: {
+        setPlayingStatus(playing, playlist) {
+            playlist.forEach((video) => {
+                if (video.id !== playing) {
+                    video.playing = false;
+                    console.log("Video not Playing is set For ", video.title, "with ID ", video.id);
+                } else {
+                    video.playing = true;
+                    console.log("Video Playing is set For ", video.title, "with ID ", video.id);
+                }
+
+            })
+
+            console.log(playlist, "   ", playing);
+            // Implement Update Player Function V Here or in Player Module
+        }
+    }
+
+    }
+
+
+
+
+</script>
+<style>
+ h1 {
+     margin-top: 1%;
+ }
+ ul {
+    list-style-type: none;
+    text-align: left;
+}
+</style>
